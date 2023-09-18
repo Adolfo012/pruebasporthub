5e05b1600a1c2d4dcc516d4a9c66821dfc55ba1e
Adolfo012 <adolfo.lamas5156@alumnos.udg.mx>
2023-09-18 16:50:30 -0600

    Tabla Usuarios

---
+++ resources/views/usuarios/formUsuarios.blade.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge"> <!-- Investigar línea -->
+    <title>Registro</title>
+
+</head>
+
+<body>
+    <h1>Registro</h1>
+    <from action="" method="post">
+        @csrf
+        
+
+
+    </from>
+
+
+
+</body>
+</html>
